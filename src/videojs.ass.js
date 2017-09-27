e9d43ae008470f765ae7e53c57c91670a6f37c15
Yme-Jan Iedema <yme-jan@iedema.me>
2017-09-27 23:43:58 +0200

    es6ified

--- src/videojs.ass.js
+++ src/videojs.ass.js
@@ -1,222 +1,208 @@
-/*! videojs-ass
- * Copyright (c) 2014 Sunny Li
- * Licensed under the Apache-2.0 license. */
+/*
+ * Author: Yme-Jan Iedema <yme-jan@iedema.me>
+ * Based off the work of Sunny Li (https://github.com/SunnyLi/videojs-ass)
+ */
 
-(function (videojs, libjass) {
-  'use strict';
+if (typeof require !== 'undefined' || typeof window.require !== 'undefined') {
+  const videojs = require('video.js');
+}
 
-  const vjs_ass = function (options) {
-    let cur_id = 0,
-      id_count = 0,
-      overlay = document.createElement('div'),
-      clocks = [],
-      clockRate = options.rate || 1,
-      delay = options.delay || 0,
-      player = this,
-      renderers = [],
-      rendererSettings = null,
-      assTrackIdMap = {},
-      tracks = player.textTracks(),
-      isTrackSwitching = false;
+if (typeof videojs === 'undefined') {
+  throw Error('videojs not found');
+}
 
-    if (!options.src) {
-      return;
+const Plugin = videojs.getPlugin('plugin');
+
+class AASSubtitles extends Plugin {
+
+  constructor(player, options) {
+    super(player, options);
+
+    this.player = player;
+    this.delay = 0;
+
+    this.overlay = this.createOverlay();
+
+    this.current_track = null;
+    this.subtitles = [];
+
+    this.is_switching = false;
+
+    player.textTracks().on('change', this.switchTrack.bind(this));
+    player.on('play', this.play.bind(this));
+    player.on('pause', this.pause.bind(this));
+    player.on('seeking', this.seeking.bind(this));
+    player.on('ratechange', this.ratechange.bind(this));
+
+    window.addEventListener('resize', this.updateDisplayArea.bind(this));
+    player.on('loadedmetadata', this.updateDisplayArea.bind(this));
+    player.on('resize', this.updateDisplayArea.bind(this));
+    player.on('fullscreenchange', this.updateDisplayArea.bind(this));
+
+
+    if (options.subtitles) {
+      for (const sub of options.subtitles) {
+        this.addSubtitle(sub.src, sub.label);
+      }
     }
 
+  }
+
+  createOverlay() {
+    const overlay = document.createElement('div');
     overlay.className = 'vjs-ass';
 
-    let OverlayComponent = {
-      name: function () {
+    const OverlayComponent = {
+      name: () => {
         return 'AssOverlay';
       },
-      el: function () {
+      el: () => {
         return overlay;
       }
     };
 
-    player.addChild(OverlayComponent, {}, 3);
+    this.player.addChild(OverlayComponent, {}, 3);
 
-    function getCurrentTime() {
-      return player.currentTime() - delay;
-    }
+    return overlay;
+  }
 
-    clocks[cur_id] = new libjass.renderers.AutoClock(getCurrentTime, 500);
+  getCurrentTime() {
+    return this.player.currentTime() - this.delay;
+  }
 
-    player.on('play', function () {
-      clocks[cur_id].play();
-    });
-
-    player.on('pause', function () {
-      clocks[cur_id].pause();
-    });
-
-    player.on('seeking', function () {
-      clocks[cur_id].seeking();
-    });
-
-    function updateClockRate() {
-      clocks[cur_id].setRate(player.playbackRate() * clockRate);
-    }
-
-    updateClockRate();
-    player.on('ratechange', updateClockRate);
-
-    function updateDisplayArea() {
-      setTimeout(function () {
-        // player might not have information on video dimensions when using external providers
-        let videoWidth = options.videoWidth || player.videoWidth() || player.el().offsetWidth,
-          videoHeight = options.videoHeight || player.videoHeight() || player.el().offsetHeight,
-          videoOffsetWidth = player.el().offsetWidth,
-          videoOffsetHeight = player.el().offsetHeight,
-
-          ratio = Math.min(videoOffsetWidth / videoWidth, videoOffsetHeight / videoHeight),
-          subsWrapperWidth = videoWidth * ratio,
-          subsWrapperHeight = videoHeight * ratio,
-          subsWrapperLeft = (videoOffsetWidth - subsWrapperWidth) / 2,
-          subsWrapperTop = (videoOffsetHeight - subsWrapperHeight) / 2;
-
-        renderers[cur_id].resize(subsWrapperWidth, subsWrapperHeight, subsWrapperLeft, subsWrapperTop);
-      }, 100);
-    }
-
-    window.addEventListener('resize', updateDisplayArea);
-    player.on('loadedmetadata', updateDisplayArea);
-    player.on('resize', updateDisplayArea);
-    player.on('fullscreenchange', updateDisplayArea);
-
-    player.on('dispose', function () {
-      for (let i = 0; i < clocks.length; i++) {
-        clocks[i].disable();
-      }
-      window.removeEventListener('resize', updateDisplayArea);
-    });
-
-    tracks.on('change', function () {
-      if (isTrackSwitching) {
-        return;
-      }
-
-      let activeTrack = this.tracks_.find(function (track) {
-        return track.mode === 'showing';
-      });
-
-      if (activeTrack) {
-        overlay.style.display = '';
-        switchTrackTo(assTrackIdMap[activeTrack.language + activeTrack.label]);
-      } else {
-        overlay.style.display = 'none';
-      }
-    });
-
-    rendererSettings = new libjass.renderers.RendererSettings();
-    libjass.ASS.fromUrl(options.src, libjass.Format.ASS).then(
-      function (ass) {
-        if (options.hasOwnProperty('enableSvg')) {
-          rendererSettings.enableSvg = options.enableSvg;
-        }
-        if (options.hasOwnProperty('fontMap')) {
-          rendererSettings.fontMap = new libjass.Map(options.fontMap);
-        } else if (options.hasOwnProperty('fontMapById')) {
-          rendererSettings.fontMap = libjass.renderers.RendererSettings
-            .makeFontMapFromStyleElement(document.getElementById(options.fontMapById));
-        }
-
-        addTrack(options.src, {label: options.label, srclang: options.srclang, switchImmediately: true});
-        renderers[cur_id] = new libjass.renderers.WebRenderer(ass, clocks[cur_id], overlay, rendererSettings);
-      }
-    );
-
-    function addTrack(url, opts) {
-      const newTrack = player.addRemoteTextTrack({
-        src: '',
-        kind: 'subtitles',
-        label: opts.label || 'ASS #' + cur_id,
-        srclang: opts.srclang || 'vjs-ass-' + cur_id,
-        default: opts.switchImmediately
-      });
-
-      assTrackIdMap[newTrack.srclang + newTrack.label] = cur_id;
-
-      if (!opts.switchImmediately) {
-        // fix multiple track selected highlight issue
-        for (let t = 0; t < tracks.length; t++) {
-          if (tracks[t].mode === 'showing') {
-            tracks[t].mode = 'showing';
-          }
-        }
-        return;
-      }
-
-      isTrackSwitching = true;
-      for (let t = 0; t < tracks.length; t++) {
-        if (tracks[t].label === newTrack.label && tracks[t].language === newTrack.srclang) {
-          if (tracks[t].mode !== 'showing') {
-            tracks[t].mode = 'showing';
-          }
-        } else {
-          if (tracks[t].mode === 'showing') {
-            tracks[t].mode = 'disabled';
-          }
-        }
-      }
-      isTrackSwitching = false;
-    }
-
-    function switchTrackTo(selected_track_id) {
-      // renderers[cur_id]._removeAllSubs();
-      renderers[cur_id]._preRenderedSubs.clear();
-      renderers[cur_id].clock.disable();
-
-      cur_id = selected_track_id;
-      if (cur_id === undefined) {
-        // case when we switche to regular non ASS closed captioning
-        return;
-      }
-
-      renderers[cur_id].clock.enable();
-      updateDisplayArea();
-      clocks[cur_id].play();
-    }
-
-    /*
-      Experimental API use at your own risk!!
-    */
-    function loadNewSubtitle(url, label, srclang, switchImmediately) {
-      const old_id = cur_id;
-      if (switchImmediately) {
-        //renderers[cur_id]._removeAllSubs();
-        renderers[cur_id]._preRenderedSubs.clear();
-        renderers[cur_id].clock.disable();
-      }
-
-      libjass.ASS.fromUrl(url, libjass.Format.ASS).then(
-        function (ass) {
-          cur_id = ++id_count;
-          clocks[cur_id] = new libjass.renderers.AutoClock(getCurrentTime, 500);
-          renderers[cur_id] = new libjass.renderers.WebRenderer(ass, clocks[cur_id], overlay, rendererSettings);
-          updateDisplayArea();
-
-          if (switchImmediately) {
-            clocks[cur_id].play();
-          } else {
-            // renderers[cur_id]._removeAllSubs();
-            renderers[cur_id]._preRenderedSubs.clear();
-            renderers[cur_id].clock.disable();
-          }
-
-          addTrack(options.src, {label: label, srclang: srclang, switchImmediately: switchImmediately});
-
-          if (!switchImmediately) {
-            cur_id = old_id;
-          }
-        }
-      );
-    }
-
-    return {
-      loadNewSubtitle: loadNewSubtitle
+  async addSubtitle(url, label, srclang = 'en') {
+    console.log('Adding track', label);
+    const track = {
+      src: '',
+      kind: 'subtitles',
+      label: label || 'Unknown',
+      srclang: srclang,
+      default: false,
+      mode: 'disabled'
     };
-  };
 
-  videojs.registerPlugin('ass', vjs_ass);
-}(window.videojs, window.libjass));
+    const ass = await libjass.ASS.fromUrl(url, libjass.Format.ASS);
+    const clock = new libjass.renderers.AutoClock(this.getCurrentTime.bind(this), 500);
+
+    const rendererSettings = new libjass.renderers.RendererSettings();
+    rendererSettings.enableSvg = false;
+    const renderer = new libjass.renderers.WebRenderer(ass, clock, this.overlay, rendererSettings);
+
+    this.subtitles.push({
+      track: this.player.addRemoteTextTrack(track, true).track,
+      clock: clock,
+      renderer: renderer,
+    });
+  }
+
+  switchTrack() {
+    if (this.is_switching) return;
+    this.is_switching = true; // recursion prevention, changing track.mode triggers track change
+    const selected_track = this.player.textTracks().tracks_.find(t => t.mode === 'showing');
+
+    if (this.current_track) {
+      this.stop(this.current_track);
+    }
+
+    if (selected_track) {
+      this.current_track = selected_track;
+      this.start(selected_track);
+    }
+
+    this.is_switching = false;
+  }
+
+  start(track) {
+    const subtitle = this.subtitles.find(e => e.track.id === track.id);
+
+    if (!subtitle) {
+      return;
+    }
+
+    console.log('Starting ', subtitle.track.label);
+    subtitle.track.mode = 'showing';
+
+    subtitle.renderer.clock.enable();
+    this.updateDisplayArea();
+    subtitle.clock.play();
+  }
+
+  stop(track) {
+    const subtitle = this.subtitles.find(e => e.track.id === track.id);
+    if (!subtitle) return;
+
+    console.log('Stopping ', subtitle.track.label);
+    subtitle.track.mode = 'disabled';
+    subtitle.renderer.clock.disable();
+
+  }
+
+  play() {
+    const sub = this.current();
+    if (!sub) return;
+    sub.clock.play();
+  }
+
+  pause() {
+    const sub = this.current();
+    if (!sub) return;
+    sub.clock.pause();
+  }
+
+  seeking() {
+    const sub = this.current();
+    if (!sub) return;
+    sub.clock.seeking();
+  }
+
+  ratechange() {
+    const sub = this.current();
+    if (!sub) return;
+    sub.clock.setRate(this.player.playbackRate());
+  }
+
+  current() {
+    if (!this.current_track) return;
+    return this.subtitles.find(e => e.track.id === this.current_track.id);
+  }
+
+  updateDisplayArea() {
+    setTimeout(() => {
+      // player might not have information on video dimensions when using external providers
+      let videoWidth = this.player.videoWidth() || this.player.el().offsetWidth,
+        videoHeight = this.player.videoHeight() || this.player.el().offsetHeight,
+        videoOffsetWidth = this.player.el().offsetWidth,
+        videoOffsetHeight = this.player.el().offsetHeight,
+
+        ratio = Math.min(videoOffsetWidth / videoWidth, videoOffsetHeight / videoHeight),
+        subsWrapperWidth = videoWidth * ratio,
+        subsWrapperHeight = videoHeight * ratio,
+        subsWrapperLeft = (videoOffsetWidth - subsWrapperWidth) / 2,
+        subsWrapperTop = (videoOffsetHeight - subsWrapperHeight) / 2;
+
+      const sub = this.current();
+      if (!sub) return;
+      console.log('resize renderer', subsWrapperWidth, subsWrapperHeight, subsWrapperLeft, subsWrapperTop);
+      sub.renderer.resize(subsWrapperWidth, subsWrapperHeight, subsWrapperLeft, subsWrapperTop);
+    }, 100);
+  }
+
+  dispose() {
+    for (const sub of this.subtitles) {
+      sub.clock.disable();
+    }
+
+    this.player.textTracks().removeEventListener('change', this.switchTrack.bind(this));
+    this.player.removeEventListener('play', this.play.bind(this));
+    this.player.removeEventListener('pause', this.pause.bind(this));
+    this.player.removeEventListener('seeking', this.seeking.bind(this));
+    this.player.removeEventListener('ratechange', this.ratechange.bind(this));
+
+    window.removeEventListener('resize', this.updateDisplayArea.bind(this));
+    this.player.removeEventListener('loadedmetadata', this.updateDisplayArea.bind(this));
+    this.player.removeEventListener('resize', this.updateDisplayArea.bind(this));
+    this.player.removeEventListener('fullscreenchange', this.updateDisplayArea.bind(this));
+  }
+}
+
+videojs.registerPlugin('ass', AASSubtitles);
